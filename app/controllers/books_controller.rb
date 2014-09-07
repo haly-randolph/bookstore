23fa1202e819acfcf51793a0f09e515745dc0c95
haly-randolph <haly.randolph@verizon.net>
2014-09-07 12:54:17 -0400

    lab 6

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,7 @@
 class BooksController < ApplicationController
    def index
    	  @available_at = Time.now
-   	  @books = ["Atlas Shrugged", "Ender's game", "Hamlet", "The Hobbit"]
+   	  #@books = ["Atlas Shrugged", "Ender's game", "Hamlet", "The Hobbit"]
+   	  @books = Book.all
    end
 end   
