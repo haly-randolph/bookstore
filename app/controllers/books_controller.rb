001e87ed6c71c76d65c1812466c6f6bc3a4d4f96
haly-randolph <haly.randolph@verizon.net>
2014-09-11 19:44:15 -0400

    class3 - Laaab3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,28 @@
 class BooksController < ApplicationController
-   def index
+    def index
    	  @available_at = Time.now
    	  @books = Book.all
-   end
+    end
 
-   def show
+    def show
       @book = Book.find(params[:id])
-   end
+    end
+
+
+	def new
+	  @book = Book.new
+	end
+
+	def create
+	  @book = Book.new(book_params)
+	  @book.save
+	  redirect_to @book
+	end
+
+	private
+
+	def book_params
+	  params.require(:book).permit(:title, :author, :pages, :price)
+	end
+
 end   
