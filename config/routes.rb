25f9d8fee0fd85e850da56d5a8ddcc448362608b
haly-randolph <haly.randolph@verizon.net>
2014-10-02 18:30:15 -0400

    class5 Lab1

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Bookstore::Application.routes.draw do
+  resources :reviews
+
   #get "/books" => "books#index", as: 'books'
   #get "/books/new" => "books#new", as: 'new_book'
   #get "/books/:id" => "books#show", as: 'book'
